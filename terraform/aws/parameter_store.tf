36c7618a0dbe7328c3d8bbd4c3d4a76a4b0e7c64
San Kim <waterfog9580@gmail.com>
2023-09-07 18:43:27 +0900

    :adhesive_bandage: Update ECR URLs in AWS Parameter Store
    Changed the value of the ECR URLs in the AWS Parameter Store from using a local ECR repository to a public ECR repository. The URL for the 'bokeh-user_service' and 'bokeh-post_service' were using the wrong repository reference, causing deployment issues. This update should correct that.

--- terraform/aws/parameter_store.tf
+++ terraform/aws/parameter_store.tf
@@ -1,11 +1,11 @@
 resource "aws_ssm_parameter" "bokeh-user_service-ecr_url" {
   name  = "bokeh-user_service-ecr_url"
   type  = "String"
-  value = aws_ecr_repository.bokeh-user_service.repository_url
+  value = aws_ecrpublic_repository.bokeh-user_service.repository_uri
 }
 
 resource "aws_ssm_parameter" "bokeh-post_service_ecr_url" {
   name  = "bokeh-post_service-ecr_url"
   type  = "String"
-  value = aws_ecr_repository.bokeh-user_service.repository_url
+  value = aws_ecrpublic_repository.bokeh-user_service.repository_uri
 }
