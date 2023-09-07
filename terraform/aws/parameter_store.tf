9dca714f0cd9f2227e8ada5de6d7fd417d4c988e
San Kim <waterfog9580@gmail.com>
2023-09-07 15:29:30 +0900

    :recycle: Correct parameter naming in AWS Parameter Store
    Changes were made to the parameters 'bokeh_user_ecr_url' and 'bokeh_post_ecr_url' in the AWS parameter store to align them with the naming conventions in use within the project. Updated names are 'bokeh-user_service-ecr_url' and 'bokeh-post_service-ecr_url' respectively. It ensures better consistency and smooth interfacing with other project modules.

--- terraform/aws/parameter_store.tf
+++ terraform/aws/parameter_store.tf
@@ -1,11 +1,11 @@
-resource "aws_ssm_parameter" "bokeh_user_ecr_url" {
-  name  = "/bokeh/user_ecr_url"
+resource "aws_ssm_parameter" "bokeh-user_service-ecr_url" {
+  name  = "bokeh-user_service-ecr_url"
   type  = "String"
   value = aws_ecr_repository.bokeh-user_service.repository_url
 }
 
-resource "aws_ssm_parameter" "bokeh_post_ecr_url" {
-  name  = "/bokeh/post_ecr_url"
+resource "aws_ssm_parameter" "bokeh-post_service_ecr_url" {
+  name  = "bokeh-post_service-ecr_url"
   type  = "String"
   value = aws_ecr_repository.bokeh-user_service.repository_url
 }
