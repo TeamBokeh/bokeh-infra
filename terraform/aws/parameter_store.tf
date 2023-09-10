79a9cbdc778b1a2382f3107ca7325cb5993cf7f3
San Kim <waterfog9580@gmail.com>
2023-09-10 16:46:29 +0900

    :bug: Update AWS SSM parameter value in Terraform script
    Changed the value of "aws_ssm_parameter" "bokeh-post_service_ecr_url" in the Terraform script. It was previously reflecting the bokeh-user_service repository URI. It's now updated with the correct bokeh-post_service repository URI. This modification ensures that the proper AWS ECR public repository is referenced in the AWS SSM parameter store.

--- terraform/aws/parameter_store.tf
+++ terraform/aws/parameter_store.tf
@@ -7,5 +7,5 @@ resource "aws_ssm_parameter" "bokeh-user_service-ecr_url" {
 resource "aws_ssm_parameter" "bokeh-post_service_ecr_url" {
   name  = "bokeh-post_service-ecr_url"
   type  = "String"
-  value = aws_ecrpublic_repository.bokeh-user_service.repository_uri
+  value = aws_ecrpublic_repository.bokeh-post_service.repository_uri
 }
