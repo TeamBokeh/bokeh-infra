43afe068d21cffbafba9e3e117d6fdae5a316687
San Kim <waterfog9580@gmail.com>
2023-09-27 18:01:18 +0900

    :fire: Rename resources to adhere to updated API naming
    The resources related to the `bokeh-user_api` have been renamed to `bokeh-user_api_rest` in both the `container_registry.tf` and `parameter_store.tf` files. This change was needed to adhere to the recent renaming of the `bokeh-user_api` to `bokeh-user_api_rest`.

--- terraform/aws/container_registry.tf
+++ terraform/aws/container_registry.tf
@@ -1,6 +1,6 @@
-resource "aws_ecrpublic_repository" "bokeh-user_api" {
+resource "aws_ecrpublic_repository" "bokeh-user_api_rest" {
   provider = aws.us-east-1
-  repository_name = "bokeh-user_api"
+  repository_name = "bokeh-user_api_rest"
 }
 
 resource "aws_ecrpublic_repository" "bokeh-post_api" {
