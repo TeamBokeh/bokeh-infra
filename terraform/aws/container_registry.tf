f36e9d2fb9d6808ecbd0560660ca7ee1aee34a87
San Kim <waterfog9580@gmail.com>
2023-09-28 09:52:50 +0900

    :adhesive_bandage: Update ECR repo and SSM parameter names
    Updated the names of the ECR repository and AWS SSM parameter for container registry in AWS from 'bokeh-post_api' to 'bokeh-post_api_rest' to better reflect the RESTful nature of the service. This change helps to avoid any confusion in future when working with these resources.

--- terraform/aws/container_registry.tf
+++ terraform/aws/container_registry.tf
@@ -3,7 +3,7 @@ resource "aws_ecrpublic_repository" "bokeh-user_api_rest" {
   repository_name = "bokeh-user_api_rest"
 }
 
-resource "aws_ecrpublic_repository" "bokeh-post_api" {
+resource "aws_ecrpublic_repository" "bokeh-post_api_rest" {
   provider = aws.us-east-1
-  repository_name = "bokeh-post_api"
+  repository_name = "bokeh-post_api_rest"
 }
--- terraform/aws/parameter_store.tf
+++ terraform/aws/parameter_store.tf
@@ -4,8 +4,8 @@ resource "aws_ssm_parameter" "bokeh-user_api_rest-ecr_url" {
   value = aws_ecrpublic_repository.bokeh-user_api_rest.repository_uri
 }
 
-resource "aws_ssm_parameter" "bokeh-post_api-ecr_url" {
-  name  = "bokeh-post_api-ecr_url"
+resource "aws_ssm_parameter" "bokeh-post_api_rest-ecr_url" {
+  name  = "bokeh-post_api_rest-ecr_url"
   type  = "String"
-  value = aws_ecrpublic_repository.bokeh-post_api.repository_uri
+  value = aws_ecrpublic_repository.bokeh-post_api_rest.repository_uri
 }
